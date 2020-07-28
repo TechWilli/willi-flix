710e211410576c11eba3d302de3f5399b41126df
William Araujo <williamf1f2@gmail.com>
2020-07-27 22:55:10 -0300

    Setup WilliFlix project

---
+++ src/components/Footer/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { FooterBase } from './styles';
+
+function Footer() {
+	return (
+		<FooterBase>
+			<a href="https://www.alura.com.br/">
+				<img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+			</a>
+			<p style={{ paddingTop: 20 }}>
+				Orgulhosamente criado durante a {' '}
+				<a href="https://www.alura.com.br/" style={{ color: '#2A7AE4'}}>Imersão React da Alura</a> por William!
+			</p>
+		</FooterBase>
+	);
+}
+
+export default Footer;
